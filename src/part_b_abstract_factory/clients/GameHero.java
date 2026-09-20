da6e354aadbfd6e31ea7906ff374c21e4debe100
diway7 <254177@astanait.edu.kz>
2026-09-20 22:11:25 +0500

    ehuuuu

--- src/part_b_abstract_factory/clients/GameHero.java
+++ src/part_b_abstract_factory/clients/GameHero.java
@@ -1,4 +1,4 @@
-package part_b_abstract_factory.client;
+package part_b_abstract_factory.clients;
 
 import part_b_abstract_factory.factory.HeroEquipmentFactory;
 import part_b_abstract_factory.model.Armor;
@@ -7,11 +7,10 @@ import part_b_abstract_factory.model.Weapon;
 public class GameHero {
     private final Weapon weapon;
     private final Armor armor;
-
-    // Клиент работает ТОЛЬКО через абстрактные интерфейсы
     public GameHero(HeroEquipmentFactory factory) {
         this.weapon = factory.createWeapon();
         this.armor = factory.createArmor();
+
     }
 
     public void prepareForBattle() {
